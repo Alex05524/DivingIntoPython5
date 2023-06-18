8b9022fb0c8258dd45e5c6bc403d31dc3f859d1b
Alexey <pwz-and_10@mail.ru>
2023-06-18 18:48:54 +0600

    create divingintopython5

---
+++ Task1.py
@@ -0,0 +1,11 @@
+import os
+
+def split_file_path(file_path):
+    path, file_name = os.path.split(file_path)
+    file_name, file_ext = os.path.splitext(file_name)
+    return path, file_name, file_ext
+
+# Пример использования
+path = "/home/user/documents/file.txt"
+result = split_file_path(path)
+print(result)
